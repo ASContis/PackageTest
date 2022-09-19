3ce9a66d166898c9506fac360609d3285f6a8b3a
Abhijit Soni <abhijit.soni@contis.com>
2022-09-19 17:16:58 +0530

    updated dependancy

--- Sample/AppDelegate.swift
+++ Sample/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import PackageLibrary
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
--- Source/PackageLibrary/Calculate.swift
+++ Source/PackageLibrary/Calculate.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Dependant
 
 public class Calculate {
     public static func add(a: Int, b:Int) -> Int {
